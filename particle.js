808ddad376b9b41cf24b5206abc6a3ebf4f9cb08
madey <89770729+K-Potatomato@users.noreply.github.com>
2021-12-17 03:09:33 +0900

    설명 최종 수정
    설명 최종 수정

--- particle.js
+++ particle.js
@@ -1,22 +1,50 @@
-class Ball {
-  constructor() {
-    this.x = random(300,width-300);
-    this.y = height-30;
-    this.s = 20;
-  }  
+
+class Particle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(random(1, 2));
+    this.acc = createVector(0, 0);
+    this.r = 10;
+    this.lifetime = 255;
+  }
+
+  finished() {
+    return this.lifetime < 0;
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  edges() {
+    if (this.pos.y >= height - this.r) {
+      this.pos.y = height - this.r;
+      this.vel.y *= -1;
+    }
+
+    if (this.pos.x >= width - this.r) {
+      this.pos.x = width - this.r;
+      this.vel.x *= -1;
+    } else if (this.pos.x <= this.r) {
+      this.pos.x = this.r;
+      this.vel.x *= -1;
+    }
+  }
 
   update() {
-	if(this.y<0){
-	ch=3;
-	}else if(height<this.y){
-	ch=-3;
-	}
-	this.y+=ch;
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+
+    this.lifetime -= 3;
   }
 
   show() {
-	noStroke();
-	fill(0);
-	ellipse(this.x, this.y, this.s, this.s);
+    stroke(255, this.lifetime);
+    strokeWeight(2);
+    fill(255, this.lifetime);
+
+    rect(this.pos.x, this.pos.y, this.r * 2);
   }
 }
